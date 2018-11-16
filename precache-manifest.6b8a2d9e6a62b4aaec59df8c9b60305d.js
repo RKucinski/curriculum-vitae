75a50639b5ace4f7d8040df13f2dab9589bf28be
Roman Kucinski <roman.kucinski@gmail.com>
2018-11-16 09:50:43 +0100

    Updates

---
+++ precache-manifest.6b8a2d9e6a62b4aaec59df8c9b60305d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a4c8680aad8ead67bab110fb3a35af",
+    "url": "/curriculum/static/media/office.f6a4c868.otf"
+  },
+  {
+    "revision": "2553e7851be1f3f6029d",
+    "url": "/curriculum/static/css/main.0e51f926.chunk.css"
+  },
+  {
+    "revision": "fd3c2b9f99b5abf822fd5de09dd68cdf",
+    "url": "/curriculum/static/media/ep_naturalblack.fd3c2b9f.png"
+  },
+  {
+    "revision": "09e6e24e3eb95a59b0e9",
+    "url": "/curriculum/static/js/1.09e6e24e.chunk.js"
+  },
+  {
+    "revision": "2fae871c0e353310d84d",
+    "url": "/curriculum/static/js/runtime~main.2fae871c.js"
+  },
+  {
+    "revision": "b2bf0ff74d24c6b0b1415525fe49f4b6",
+    "url": "/curriculum/static/media/en.b2bf0ff7.jpg"
+  },
+  {
+    "revision": "2553e7851be1f3f6029d",
+    "url": "/curriculum/static/js/main.2553e785.chunk.js"
+  },
+  {
+    "revision": "9625b7f49fac861cd3ab1ce7804f9171",
+    "url": "/curriculum/static/media/photo.9625b7f4.png"
+  },
+  {
+    "revision": "99477b4393329d9294204a73f9881144",
+    "url": "/curriculum/static/media/linkedin_logo.99477b43.png"
+  },
+  {
+    "revision": "9cab3941a9641e93c85fb1b45f2da6a2",
+    "url": "/curriculum/static/media/github.9cab3941.png"
+  },
+  {
+    "revision": "09e6e24e3eb95a59b0e9",
+    "url": "/curriculum/static/css/1.9e0be2c4.chunk.css"
+  },
+  {
+    "revision": "a42426b6ce5076bbc36e65b29820137a",
+    "url": "/curriculum/index.html"
+  }
+];
