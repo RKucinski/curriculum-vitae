a117ee4621a25e3fcfc2784b532a11ab14347ed3
Roman Kucinski <roman.kucinski@gmail.com>
2018-11-16 10:15:18 +0100

    last

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
       <div className="contact">
         <h3 style={{ fontFamily: "'office', sans-serif", textAlign: "center" }}>Contact</h3>
         <div className="contactImg">
-          <a href="https://www.linkedin.com/in/roman-kucinski-270b10bb/" target='blank'><img src={LinkedIn} alt="contact me"/></a>
+          <a href="www.linkedin.com/in/romankucinski" target='blank'><img src={LinkedIn} alt="contact me"/></a>
           <a href="https://github.com/RKucinski" target='blank'><img src={github} alt="contact me"/></a>
         </div>
       </div>
